1e1744576ac92c4e8c1d420db61b05ec044ecd17
MIKEL ARICETA <mikelariceta@MIKELs-MacBook-Pro.local>
2022-03-15 22:53:41 +0100

    Create and test transfer function

--- contracts/BaraToken.sol
+++ contracts/BaraToken.sol
@@ -7,11 +7,30 @@ contract BaraToken {
     string public name = "Mikel";
     string public symbol = "BARA";
     string public standard = "BARA Token v.10";
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 value
+    );
+
     mapping(address => uint256) public balanceOf;
 
     constructor (uint256 _initialSupply) {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply; //1 million
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        require(balanceOf[msg.sender] >= _value);
+        
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+
     }
 
 }
